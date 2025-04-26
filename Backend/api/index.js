7612ad63175bf9e75268b1c542189b0cfe540c0b
Mahesh Babu . Karna <karnamaheshbabu32@gmail.com>
2025-04-26 14:43:50 +0530

    Create index.js

---
+++ Backend/api/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.get('/api/capture-screenshot', async (req, res) => {
+  try {
+    const browser = await puppeteer.launch({ headless: "new" });
+    const page = await browser.newPage();
+
+    await page.goto('https://infographic32.vercel.app', {
+      waitUntil: 'domcontentloaded',
+      timeout: 3000
+    });
+
+    const screenshot = await page.screenshot({ type: 'png', fullPage: true });
+    await browser.close();
+
+    res.set('Content-Type', 'image/png');
+    res.send(screenshot);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Screenshot failed');
+  }
+});
+
+// IMPORTANT: instead of app.listen(), export the app
+module.exports = app;
